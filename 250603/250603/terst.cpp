e171c3051c80cca27740f6c4a7a269257fb0fb2d
Sheep-aron <kissing0927@gmail.com>
2025-06-04 15:14:14 +0800

    C语言算法练习

--- 250603/250603/terst.cpp
+++ 250603/250603/terst.cpp
@@ -1,15 +1,46 @@
-#include <iostream>
+//JZ11 旋转数组的最小数字
+//class Solution
+//{
+//public:
+//    int minNumberInRotateArray(vector<int>& nums)
+//    {
+//        int len = nums.size(), left = 0, right = len - 1;
+//        while (left < right)
+//        {
+//            int mid = left + (right - left) / 2;
+//            if (nums[mid] > nums[right])
+//                left = mid + 1;
+//            else if (nums[mid] < nums[right])
+//                right = mid;
+//            else
+//                --right;
+//        }
+//        return nums[left];
+//    }
+//};
 
-#include <vector>
-
-int main()
-{
-	std::vector<int> v(10, 1); // Initialize a vector of size 10 with all elements set to 1
-	int sum = 0;
-	for (int i = 0; i < v.size(); ++i) {
-		sum += v[i]; // Sum all elements in the vector
-	}
-	std::cout << "Sum of elements: " << sum << std::endl; // Output the sum
-	// Return 0 to indicate successful execution
-	return 0;
-}
+//645. 错误的集合
+//class Solution
+//{
+//public:
+//    vector<int> findErrorNums(vector<int>& nums)
+//    {
+//        vector<int> ErrorNums(2);
+//        sort(nums.begin(), nums.end());
+//        int len = nums.size(), prev = 0;
+//        for (int i = 0; i < nums.size(); ++i)
+//        {
+//            int curr = nums[i];
+//            if (prev == curr)
+//                ErrorNums[0] = prev;
+//            else if (curr - prev > 1)
+//                ErrorNums[1] = prev + 1;
+//
+//            if (nums[len - 1] != len)
+//                ErrorNums[1] = len;
+//
+//            prev = curr;
+//        }
+//        return ErrorNums;
+//    }
+//};
