3eddb87bc15dfc7c17236e147cb97d84a6d1a2af
Sheep-aron <kissing0927@gmail.com>
2025-06-18 23:56:10 +0800

    C语言练习

---
+++ 250618/250618/testcpp.cpp
@@ -0,0 +1,45 @@
+// BC6 小飞机
+//#include <iostream>
+//
+//int main()
+//{
+//    std::cout << "     **\n";
+//    std::cout << "     **\n";
+//    std::cout << "************\n";
+//    std::cout << "************\n";
+//    std::cout << "    *  *\n";
+//    std::cout << "    *  *\n";
+//    return 0;
+//}
+
+// BC41 你是天才吗？
+//#include <iostream>
+//
+//int main()
+//{
+//    int num = 0;
+//    std::cin >> num;
+//    if (num >= 140)
+//        std::cout << "Genius";
+//    return 0;
+//}
+
+// BC32 输出学生信息
+//#include <iostream>
+//
+//int main()
+//{
+//    std::cout << "Name    Age    Gender" << std::endl;
+//    std::cout << "---------------------" << std::endl;
+//    std::cout << "Jack    18     man" << std::endl;
+//    return 0;
+//}
+
+// BC31 发布信息
+//#include <iostream>
+//
+//int main()
+//{
+//	std::cout << "I lost my cellphone!" << std::endl;
+//	return 0;
+//}
