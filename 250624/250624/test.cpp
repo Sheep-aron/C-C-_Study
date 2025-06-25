0914a73218656f8ab38a1d647c76d15566dd483c
Sheep-aron <kissing0927@gmail.com>
2025-06-25 17:55:20 +0800

    练习

---
+++ 250624/250624/test.cpp
@@ -0,0 +1,13 @@
+//BC25 计算体重指数
+//#include <iostream>
+//#include <iomanip>
+//
+//int main()
+//{
+//    double height = 0.0, weight = 0.0;
+//    std::cin >> weight >> height;
+//    double BMI = weight / ((height / 100) * (height / 100));
+//    std::cout << std::fixed << std::setprecision(2) << BMI;
+//    return 0;
+//}
+
