f9ad4bb813670783e6f5179c871091e73e129a08
Sheep-aron <kissing0927@gmail.com>
2025-06-26 15:25:48 +0800

    练习

---
+++ 250625/250625/test.cpp
@@ -0,0 +1,27 @@
+//BC22 你能活多少秒
+//#include <iostream>
+//
+//int main()
+//{
+//    long int seconds = 31560000, year = 0;
+//    std::cin >> year;
+//    seconds *= year;
+//    std::cout << seconds;
+//
+//    return 0;
+//}
+
+//BC113 小乐乐定闹钟
+//#include <iostream>
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int hour = 0, minute = 0, k = 0;
+//    scanf("%d:%d %d", &hour, &minute, &k);
+//    minute += k;
+//    hour += minute / 60;
+//    minute = minute % 60;
+//    printf("%02d:%02d", hour % 24, minute);
+//    return 0;
+//}
