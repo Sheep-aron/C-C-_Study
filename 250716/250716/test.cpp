51d6fa9548df9cee6b32226fc7272e2151605621
Sheep-aron <kissing0927@gmail.com>
2025-07-17 00:04:13 +0800

    练习

---
+++ 250716/250716/test.cpp
@@ -0,0 +1,39 @@
+//打印水仙花数
+//#include <iostream>
+//#include <string>
+//
+//int daffNum(int num)
+//{
+//	int len = std::to_string(num).length(), sum = 0;
+//	while (num)
+//	{
+//		int digit = num % 10;
+//		sum += pow(digit, len);
+//		num /= 10;
+//	}
+//	return sum;
+//}
+//
+//int main()
+//{
+//	for (int i = 0; i < 100000; ++i)
+//	{
+//		if (daffNum(i) == i)
+//			std::cout << i << " ";
+//	}
+//	return 0;
+//}
+
+
+#include <iostream>
+
+int main()
+{
+	int a = 0;
+	std::cin >> a;
+	for (int i = 0; i < 5; ++i)
+	{
+
+	}
+	return 0;
+}
